e0c591e57c14cbe49b4b60cfef9279eb121432f5
Sanjid Khan <sksanjid295@gmail.com>
2026-09-18 03:34:39 +0600

    basic of node js

---
+++ Day01/second.js
@@ -0,0 +1,20 @@
+
+console.log("hello I am second");
+
+function  sum(a,b){
+    console.log(a+b);
+}
+
+function  sub(a,b){
+    console.log(a-b);
+}
+
+console.log(module.exports);
+
+// module.exports={sum,sub};
+
+module.exports.sum=sum;
+module.exports.sub=sub;
+
+// {sum:sum
+//  sub:sub}
---
+++ Day01/sour/first.js
@@ -0,0 +1,5 @@
+import sum from "./second.js";
+
+sum(3,8);
+
+console.log("Hello World");
